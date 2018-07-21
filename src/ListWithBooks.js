f01519f114cd6ff57a422485b3aada288b2c4654
Panayot Angarev <pancho.angarev@gmail.com>
2018-07-22 00:54:07 +0300

    Add SigleBook.js

--- src/ListWithBooks.js
+++ src/ListWithBooks.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import SingleBook from './SingleBook';
 
 class ListWithBooks extends Component {
   render() {
@@ -19,7 +20,7 @@ class ListWithBooks extends Component {
           </div>
           {Object.keys(shelves).map((shelf) => {
             return (
-                <div className="list-books-content">
+                <div key={shelf} className="list-books-content">
                   <div>
                     <div className="bookshelf">
                       <h2 className="bookshelf-title">{shelves[shelf]}</h2>
@@ -27,40 +28,9 @@ class ListWithBooks extends Component {
                         <ol className="books-grid">
                           {books.filter(book => book.shelf === shelf).map((book) => {
                             return (
-                                <li key={book.id}>
-                                  <div className="book">
-                                    <div className="book-top">
-                                      <div className="book-cover" style={{
-                                        width: 128,
-                                        height: 193,
-                                        backgroundImage: `url("${book.imageLinks.smallThumbnail}")`,
-                                      }}></div>
-                                      <div className="book-shelf-changer">
-                                        <select>
-                                          <option value="move" disabled>Move
-                                            to...
-                                          </option>
-                                          <option
-                                              value="currentlyReading">Currently
-                                            Reading
-                                          </option>
-                                          <option value="wantToRead">Want to
-                                            Read
-                                          </option>
-                                          <option value="read">Read</option>
-                                          <option value="none">None</option>
-                                        </select>
-                                      </div>
-                                    </div>
-                                    <div
-                                        className="book-title">{book.title}</div>
-                                    <div
-                                        className="book-authors">{book.authors}</div>
-                                  </div>
-                                </li>
-                            );
-                          })
-
+                                <SingleBook key={book.title} book={book}/>
+                              );
+                            })
                           }
                         </ol>
                       </div>
@@ -69,8 +39,6 @@ class ListWithBooks extends Component {
                 </div>
             );
           })}
-
-
           <div className="open-search">
             {/*Add link to search page*/}
             <Link to='/search' title='Add book'>Add Book</Link>
